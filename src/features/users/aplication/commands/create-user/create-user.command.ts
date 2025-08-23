636c0ad9d737a1fd91cdd2b4dea6b26728328216
Klever Ferreira <kleverfp@hotmail.com>
2025-08-23 08:54:11 -0300

    feat: init

--- src/features/users/aplication/commands/create-user/create-user.command.ts
+++
@@ -1,6 +0,0 @@
-export class CreateUserCommand {
-  constructor(
-    public readonly name: string,
-    public readonly email: string,
-  ) {}
-}
--- src/shared/domain/domain.event.ts
+++
@@ -1,9 +0,0 @@
-export abstract class DomainEvent {
-  public readonly occurredOn: Date;
-  public readonly eventName: string;
-
-  constructor(eventName: string) {
-    this.occurredOn = new Date();
-    this.eventName = eventName;
-  }
-}
